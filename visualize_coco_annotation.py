cac2a04ba206f7b2b33eb31fdc6335e651937bba
Hana <41535875+Hanawh@users.noreply.github.com>
2020-06-15 19:05:42 +0800

    visualize_coco_annotation

---
+++ visualize_coco_annotation.py
@@ -0,0 +1,156 @@
+import os
+import json
+from matplotlib import colors
+import cv2
+import numpy as np
+from pycocotools.coco import COCO
+import webcolors
+import random
+import ipdb
+
+# please pre-defines before u use this script
+
+PRE_DEFINE_CATEGORIES =  ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+           'fire hydrant', '', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep',
+           'cow', 'elephant', 'bear', 'zebra', 'giraffe', '', 'backpack', 'umbrella', '', '', 'handbag', 'tie',
+           'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+           'skateboard', 'surfboard', 'tennis racket', 'bottle', '', 'wine glass', 'cup', 'fork', 'knife', 'spoon',
+           'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut',
+           'cake', 'chair', 'couch', 'potted plant', 'bed', '', 'dining table', '', '', 'toilet', '', 'tv',
+           'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink',
+           'refrigerator', '', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+           'toothbrush']
+
+
+STANDARD_COLORS = [
+    'LawnGreen', 'Chartreuse', 'Aqua','Beige', 'Azure','BlanchedAlmond','Bisque',
+    'Aquamarine', 'BlueViolet', 'BurlyWood', 'CadetBlue', 'AntiqueWhite',
+    'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan',
+    'DarkCyan', 'DarkGoldenRod', 'DarkGrey', 'DarkKhaki', 'DarkOrange',
+    'DarkOrchid', 'DarkSalmon', 'DarkSeaGreen', 'DarkTurquoise', 'DarkViolet',
+    'DeepPink', 'DeepSkyBlue', 'DodgerBlue', 'FireBrick', 'FloralWhite',
+    'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod',
+    'Salmon', 'Tan', 'HoneyDew', 'HotPink', 'IndianRed', 'Ivory', 'Khaki',
+    'Lavender', 'LavenderBlush', 'AliceBlue', 'LemonChiffon', 'LightBlue',
+    'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey',
+    'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue',
+    'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime',
+    'LimeGreen', 'Linen', 'Magenta', 'MediumAquaMarine', 'MediumOrchid',
+    'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen',
+    'MediumTurquoise', 'MediumVioletRed', 'MintCream', 'MistyRose', 'Moccasin',
+    'NavajoWhite', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed',
+    'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed',
+    'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple',
+    'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Green', 'SandyBrown',
+    'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue',
+    'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'GreenYellow',
+    'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White',
+    'WhiteSmoke', 'Yellow', 'YellowGreen'
+]
+
+def from_colorname_to_bgr(color):
+    rgb_color=webcolors.name_to_rgb(color)
+    result=(rgb_color.blue,rgb_color.green,rgb_color.red)
+    return result
+
+# process the custom dataset json file which is coco fomat
+class Dataset():
+    def __init__(self, json_file, image_dir, output_dir):
+        self.json_file = json_file
+        self.image_dir = image_dir
+        self.output_dir = output_dir
+
+        self.coco = COCO(json_file)
+        self.image_ids = self.coco.getImgIds()
+    
+    def len(self):
+        return len(self.image_ids)
+
+    def getitem(self, idx):
+        img, file_name = self.load_image(idx)
+        annot = self.load_annotations(idx)
+        sample = {'img': img, 'annot': annot}
+        return sample, file_name
+    
+    def load_image(self, image_index):
+        image_info = self.coco.loadImgs(self.image_ids[image_index])[0]
+        path = os.path.join(self.image_dir, image_info['file_name'])
+        img = cv2.imread(path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+        return img, image_info['file_name']
+    
+    def load_annotations(self, image_index):
+        # get ground truth annotations
+        annotations_ids = self.coco.getAnnIds(imgIds=self.image_ids[image_index], iscrowd=False)
+        annotations = np.zeros((0, 5))
+
+        # some images appear to miss annotations
+        if len(annotations_ids) == 0:
+            return annotations
+
+        # parse annotations
+        coco_annotations = self.coco.loadAnns(annotations_ids)
+        for idx, a in enumerate(coco_annotations):
+
+            # some annotations have basically no width / height, skip them
+            if a['bbox'][2] < 1 or a['bbox'][3] < 1:
+                continue
+
+            annotation = np.zeros((1, 5))
+            annotation[0, :4] = a['bbox']
+            annotation[0, 4] = a['category_id'] - 1
+            annotations = np.append(annotations, annotation, axis=0)
+
+        # transform from [x, y, w, h] to [x1, y1, x2, y2]
+        annotations[:, 2] = annotations[:, 0] + annotations[:, 2]
+        annotations[:, 3] = annotations[:, 1] + annotations[:, 3]
+
+        return annotations
+    
+    def drawBox(self, image_index):
+        sample, file_name = self.getitem(image_index)
+        img, annots = sample['img'], sample['annot']
+        for annot in annots:
+            x1, y1, x2, y2 = annot[:4]
+            class_name = PRE_DEFINE_CATEGORIES[int(annot[4])]
+            self.plot_one_box(img, [x1, y1, x2, y2], label=class_name, color=from_colorname_to_bgr(STANDARD_COLORS[int(annot[4])]))
+        cv2.imwrite(f'{self.output_dir}/{file_name}', img)
+
+    
+    def plot_one_box(self, img, coord, label, color):
+        tl = int(round(0.001 * max(img.shape[0:2]))) # box font thickness
+        c1, c2 = (int(coord[0]), int(coord[1])), (int(coord[2]), int(coord[3]))
+        cv2.rectangle(img, c1, c2, color, thickness=tl)
+        
+        tf = max(tl - 2, 1)  # class font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=float(tl) / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0]+15, c1[1] - t_size[1] -3
+        cv2.rectangle(img, c1, c2 , color, -1)  # filled
+        cv2.putText(img, '{}'.format(label), (c1[0],c1[1] - 2), 0, float(tl) / 3, [0, 0, 0], thickness=tf, lineType=cv2.FONT_HERSHEY_SIMPLEX)
+
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="Visualize COCO format annotation."
+    )
+    parser.add_argument("json_file", help="Input COCO format json file.", type=str)
+    parser.add_argument("image_dir", help="Directory path to images.", type=str)
+    parser.add_argument("random_number", help="How many imgs randomly selected.", type=int)
+    parser.add_argument("--output_dir", default="visualize/", help="Directory path to store images.", type=str)
+    args = parser.parse_args()
+
+    mydata = Dataset(args.json_file, args.image_dir, args.output_dir)
+
+    if os.path.exists(args.output_dir) is None:
+        os.makedirs(args.output_dir)
+    
+    length = mydata.len()
+    index = random.sample(list(np.arange(0, length)), args.random_number)
+    
+    for ind in index:
+        mydata.drawBox(ind)
+
+    
+
